cae430aeeba5868281026e6c2d47763a5b51bde7
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-04 11:20:29 +0530

    Create 10.java

---
+++ Patterns/10.java
@@ -0,0 +1,22 @@
+public class PrintPattern {
+
+    public static void main(String[] args) {
+        int rows = 5;
+
+        // Print pyramid
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Print inverted pyramid
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
