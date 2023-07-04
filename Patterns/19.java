77df13612007bf571852c9c95ee969723185af41
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-04 11:30:26 +0530

    Create 19.java

---
+++ Patterns/19.java
@@ -0,0 +1,26 @@
+public class PrintPattern {
+
+    public static void main(String[] args) {
+        int rows = 9;
+        int midRow = (rows + 1) / 2;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if (j <= midRow) {
+                    if (j <= (midRow - i + 1)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    if (j >= (midRow + i - 1)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
