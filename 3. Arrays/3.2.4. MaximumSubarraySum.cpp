3a8111a0f715eb474afa5fc69ed1f1f0872ea707
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-08-18 19:12:42 +0530

    Create 3.2.4. MaximumSubarraySum.cpp

---
+++ 3. Arrays/3.2.4. MaximumSubarraySum.cpp
@@ -0,0 +1,127 @@
+// Kadane's Algorithm
+
+/*
+Kadane’s Algorithm : Maximum Subarray Sum in an Array
+Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which
+has the largest sum and returns its sum and prints the subarray.
+
+Examples
+Example 1:
+
+Input: arr = [-2,1,-3,4,-1,2,1,-5,4] 
+
+Output: 6 
+
+Explanation: [4,-1,2,1] has the largest sum = 6. 
+
+Examples 2: 
+
+Input: arr = [1] 
+
+Output: 1 
+
+Explanation: Array has only one element and which is giving positive sum of 1. 
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void MaxSub(int arr[], int n)
+{
+    long long max = LONG_LONG_MIN;
+    long sum = 0;
+
+    for (int i=0; i<n; i++) {
+        sum += arr[i];
+
+        if (sum > max)
+            max = sum;
+        if (sum < 0)
+            sum = 0;
+    }
+    cout << "Maximum contiguous sum is: " << max << endl; 
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of Array: ";
+    cin >> n;
+    int arr[n]={0};
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    MaxSub(arr, n);
+    return 0;
+}
+
+/*
+Follow Up Question:
+Algorithm / Intuition
+There might be more than one subarray with the maximum sum. We need to print any of them.
+
+Intuition: Our approach is to store the starting index and the ending index of the subarray. Thus we can easily get the subarray afterward without actually storing the subarray elements.
+
+If we carefully observe our algorithm, we can notice that the subarray always starts at the particular index where the sum variable is equal to 0, and at the ending index, the sum always crosses the previous maximum sum(i.e. maxi).
+
+So, we will keep a track of the starting index inside the loop using a start variable.
+We will take two variables ansStart and ansEnd initialized with -1. And when the sum crosses the maximum sum, we will set ansStart to the start variable and ansEnd to the current index i.e. i.
+The rest of the approach will be the same as Kadane’s Algorithm.
+*/
+
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long maxSubarraySum(int arr[], int n) {
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    int start = 0;
+    int ansStart = -1, ansEnd = -1;
+    for (int i = 0; i < n; i++) {
+
+        if (sum == 0) start = i; // starting index
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    //printing the subarray:
+    cout << "The subarray is: [";
+    for (int i = ansStart; i <= ansEnd; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "]n";
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    long long maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+*/
