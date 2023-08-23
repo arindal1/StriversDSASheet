e90e87c77423e81aacb9db0fdce7b0ec5bd7e6a0
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-08-23 21:27:34 +0530

    Create 4.1.1. BinarySearchIterative.cpp

---
+++ 4. Binary Search/1. BS on 1D Arrays/4.1.1. BinarySearchIterative.cpp
@@ -0,0 +1,45 @@
+/*
+Problem statement: You are given a sorted array of integers and a target, your task is to search for the target in the given array. Assume the given array does not contain any duplicate numbers.
+
+Let’s say the given array is = {3, 4, 6, 7, 9, 12, 16, 17} and target = 6.
+*/
+
+// ITERATIVE APPROACH
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int target, int n) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (target > nums[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of Array: ";
+    cin >> n;
+    vector<int> arr;
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    int target = 6;
+    int ind = binarySearch(arr, target, n);
+    if (ind == -1)
+        cout << "The target is not present." << endl;
+    else
+        cout << "The target is at index: " << ind << endl;
+    return 0;
+}
